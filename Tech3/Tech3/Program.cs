3afb8c4a800d814a7f511a5d2da2f296682481a8
SimasBand <70725324+SimasBand@users.noreply.github.com>
2020-09-24 01:08:21 -0400

    Add files via upload

---
+++ Tech3/Tech3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tech3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello What is your name?: ");
+            String name = Console.ReadLine();
+            Console.Write("Hello, ");
+            Console.Write(name);
+            Console.WriteLine("!");
+        }
+    }
+}
